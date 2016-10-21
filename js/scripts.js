8f4b4a4a3ac948ca00f033939404c8183d37b164
SuziRubino <suzi@suzirubi.com>
2016-10-21 13:58:33 -0700

    debugg javascript issues, not complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,46 @@
-$(function() {
+$(document).ready(function(){
+//move this function to business logic and assign var to function to call down here
+  var userCareerTrack;
+
+
+
+  var calculate = function(whatEnjoyWeek1Input, whatEnjoyWeek2Input, courseDesInput, workQuesInput, salaryQuesInput) {
+
+
+if (whatEnjoyWeek1Input + whatEnjoyWeek2Input + courseDesInput + workQuesInput + salaryQuesInput <= 5) {
+
+$("#userCareerTrack").text("Your track is problaby back end");
+
+    } else if (whatEnjoyWeek1Input + whatEnjoyWeek2Input + courseDesInput + workQuesInput + salaryQuesInput > 5) {
+      $("#userCareerTrack").text("Your track is problaby back end");
+    } else if (whatEnjoyWeek1Input + whatEnjoyWeek2Input + courseDesInput + workQuesInput + salaryQuesInput >= 10) {
+      $("#userCareerTrack").text("Still deciding");
+
+    } else {
+      $("#userCareerTrack").text("Do more research, but we'd have a beer with you and talk about it!");
+      }
+  }
+
+
+
 //copy this function below for letter form with id
   $("form#careerTrack").submit(function(event) {
+        event.preventDefault();
     var whatEnjoyWeek1Input = parseInt($("select#whatEnjoyWeek1").val());
     var whatEnjoyWeek2Input = parseInt($("select#whatEnjoyWeek2").val());
-    var coursDesInput = parseInt($("select#coursDes").val());
+    var courseDesInput = parseInt($("select#courseDes").val());
     var workQuesInput = parseInt($("select#workQues").val());
     var salaryQuesInput = parseInt($("select#salaryQues").val());
 
-//move this function to business logic and assign var to function to call down here
-    if (!whatEnjoyWeek1Input || !whatEnjoyWeek2Input || !coursDesInput || !workQuesInput || !salaryQuesInput) {
-      alert("Please answer each question");
-    } else if (wallInput = 2) {
-      alert("You should NOT vote");
-    } else if (wallInput + emailInput + putinInput < 3) {
-        alert("You should NOT vote");
-    } else {
-        alert("You should vote for Hillary");
-      }
 
 
-    event.preventDefault();
+    calculate (userCareerTrack);
+    alert(userCareerTrack);
+    console.log(whatEnjoyWeek1Input + whatEnjoyWeek2Input + courseDesInput + workQuesInput + salaryQuesInput);
+
+
+
+
   });
+
 });
