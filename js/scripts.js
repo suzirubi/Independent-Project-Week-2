508fb7f30bc7d9010c030b9a464ded86224dbbfe
SuziRubino <suzi@suzirubi.com>
2016-10-21 11:29:25 -0700

    start script business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 $(function() {
 //copy this function below for letter form with id
   $("form#careerTrack").submit(function(event) {
-    var wallInput = parseInt($("select#wall").val());
-    var emailInput = parseInt($("select#email").val());
-    var putinInput = parseInt($("select#putin").val());
+    var whatEnjoyWeek1Input = parseInt($("select#whatEnjoyWeek1").val());
+    var whatEnjoyWeek2Input = parseInt($("select#whatEnjoyWeek2").val());
+    var coursDesInput = parseInt($("select#coursDes").val());
+    var workQuesInput = parseInt($("select#workQues").val());
+    var salaryQuesInput = parseInt($("select#salaryQues").val());
+
 //move this function to business logic and assign var to function to call down here
-    if (!wallInput || !emailInput || !putinInput) {
+    if (!whatEnjoyWeek1Input || !whatEnjoyWeek2Input || !coursDesInput || !workQuesInput || !salaryQuesInput) {
       alert("Please answer each question");
     } else if (wallInput = 2) {
       alert("You should NOT vote");
